562074173e81083ebb50f0c3f841411a1bc1147d
Domas <domastoli@gmail.com>
2023-03-30 11:55:18 +0300

    Strings

--- js/02-strings.js
+++ js/02-strings.js
@@ -2,7 +2,75 @@ console.clear();
 
 /*
 Strings - simboliu sarasas/junginys, tekstas ->paragrafai ->sakiniu ->zodziu
+
+
+Kintamuju iniciavimas:
+-const (default)
+-let (tik jei yra poreikis keist reiksme)
+-var (nenaudoti, nebent TIKRAI zinote ka darote)
+
+Sting'o iniciavimas:
+-viengubos kabutes
+dvigubos kabutes
+
 */
+// KINTAMUJU INICIAVIMAS
+
+const vardas = 'Petras';
+console.log(vardas);
+
+const keturi = 4;
+console.log(keturi);
+
+const keturitekstu = '4';
+console.log(keturitekstu);
 
 console.log('Labas');
 console.log('Labas rytas');
+
+pinigine = 5;
+console.log(pinigine);
+
+//Strings
+
+const firstname = 'Maryte';
+console.log(firstname);
+
+const lastname = 'Marytaite';
+console.log(lastname);
+
+//Labas rytas, Maryte!
+
+console.log('Labas rytas, Maryte!')
+
+console.log('Labas rytas,', firstname)
+
+console.log('Labas rytas,', firstname, '!');
+
+const pasisveikinimas = 'Labas rytas, ' + firstname + '!';
+console.log(pasisveikinimas);
+
+//string + string
+//numger + number
+//string + number
+//number + string
+// Sveiki, mano vardas Jonas ir man 88 metai!
+
+const name1 = 'Jonas';
+const age1 = 88;
+
+const prisistatymas = 'Sveiki, mano vardas ' + name1 + ' ir man ' + age1 + ' metai!';
+console.log(prisistatymas);
+
+//81 / 3 -> 27
+
+const x = '4';
+const y = 5;
+
+console.log(x + y);
+
+console.log(x - y);
+
+console.log(x / y);
+
+console.log(x * y);
