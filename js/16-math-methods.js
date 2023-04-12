fe1b3644699af67f21b29e0349317f230da584be
Domas <domastoli@gmail.com>
2023-04-12 14:23:36 +0300

    Math methods

---
+++ js/16-math-methods.js
@@ -0,0 +1,37 @@
+console.clear();
+
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+
+console.log(Math.abs(-7));
+console.log(Math.abs(7));
+
+console.log(Math.ceil(5));
+console.log(Math.ceil(5.5));
+console.log(Math.ceil(5.1));
+
+console.log(Math.floor(9));
+console.log(Math.floor(9.5));
+console.log(Math.floor(9.1));
+
+console.log(Math.round(1.5));
+console.log(Math.round(1.0001));
+console.log(Math.round(-1.5));
+console.log(Math.round(-1.51));
+
+console.log(Math.exp(2, 3));
+console.log(Math.max(2, 3, 5, 1, -7));
+console.log(Math.min(2, 3, 5, 1, -7));
+
+console.log(Math.pow(2, 3), 2 ** 3);
+
+console.log(Math.sqrt(9));
+console.log(Math.sqrt(-1));
+
+console.log(Math.trunc(-2));
+
+
+
+
+
